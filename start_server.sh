d5c34c9562674b6462bff4a59ae32569233e5877
seieibob <joncole04412@gmail.com>
2017-03-22 14:08:25 -0400

    Fix string formatting

--- start_server.sh
+++ start_server.sh
@@ -19,7 +19,7 @@ fi
 
 # Abort if the dev.properties file does not exist
 if [ ! -f ./dev.properties ]; then
-    echo "'./dev.properties' not found! Setting up from template."
+    echo "./dev.properties not found! Setting up from template."
     cp ./template.properties ./dev.properties
     echo "Make sure to fill in the address you want this server to reside on, then run this command again."
     exit
@@ -27,7 +27,7 @@ fi
 
 # Abort if the dev.properties file is not configured
 if [ "$(prop 'app.server.address')" = "YOUR_ADDRESS_HERE"]; then
-    echo "The address in './dev.properties' is not configured. Please fill this in and run this command again."
+    echo "The address in ./dev.properties is not configured. Please fill this in and run this command again."
     exit
 fi
 
