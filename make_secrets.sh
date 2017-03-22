7456abfa4d66e9ab78ac829ab943022c0b162264
seieibob <joncole04412@gmail.com>
2017-03-22 14:13:46 -0400

    Fix make_secrets hang

--- make_secrets.sh
+++ make_secrets.sh
@@ -3,20 +3,18 @@
 # From https://irb.rocks/create-secrets-yml-file-dynamically-with-bash/
 
 if [ ! -f ./config/secrets.yml ]; then
-  echo -e "\nGenerating a secrets.yml file"
+  echo -e "Generating a secrets.yml file"
 
   # Random Keys
   KEY_DEV=$(bin/rake secret)
   KEY_TEST=$(bin/rake secret)
 
   # Generate the file
-  cat > ./config/secrets.yml << EOL
+  cat > ./config/secrets.yml <<EOL
 development:
   secret_key_base: ${KEY_DEV}
-
 test:
   secret_key_base: ${KEY_TEST}
-
 production:
   secret_key_base: <%= ENV["SECRET_KEY_BASE"] %>
 EOL
