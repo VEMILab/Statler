0ed1894e19dc56ceaa0e459a9106255375e91886
kendra <kendra.bird@umit.maine.edu>
2017-03-28 16:12:33 -0400

    Commit changes?

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -8,18 +8,9 @@ class Annotation < ActiveRecord::Base
 	#this should be searching itself, with the search parameter submitted by the user	
 	def self.search(search)
 	  #query to identify annotations where the 'annotation' field contains a partial or complete string resembling the user query
-	##if search.respond_to?(:to_i)	  
-##	elsif search.respond_to?(:to_s)
-		where ("annotation LIKE %#{search}% OR id LIKE %#{search}%")
+	where("annotation like ?", "%#{search}%")
 
 	end #end of search()
 	
-	#query to identify annotations by id
-#	def self.search(id_search)
-##	  where (:id == "%#{search}%")
-##	end
-	##def self.to_s
-	##	"Name: #{self.name} Video: #{:video} Author: #{:user} Tags: #{:tags}"
-	##end #end of to_s
-	
+
 end
