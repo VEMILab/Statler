e99f725dae5736bf3c2ecc8417052bd218e414b9
seieibob <joncole04412@gmail.com>
2017-03-28 22:35:01 -0400

    Try function reuse

--- app/controllers/annotators_helper.rb
+++ app/controllers/annotators_helper.rb
@@ -150,79 +150,7 @@ def editAnnotation ## accepts annotation id
 	edit_anno.update(deprecated: true)
     
 	# Create a new annotation linking back to the old one.
-  @x = params[:annotation]
-	  
-  @annotation = Annotation.new
-  @annotation.annotation = params[:annotation]
-  @annotation.pointsArray = params[:pointsArray]
-  @annotation.beginTime = params[:beginTime]
-  @annotation.endTime = params[:endTime]
-  #@annotation.tags = params[:tags]
-  @annotation.user_id = nil#session[:user_id]
-  if params[:id]  ## if an old annotation id is supllied, this is an edit and we should create a pointer to the old annotation
-    @annotation.prev_anno_ID = params[:id]
-  end
-    
-  @videos = [] 
-  @location = params[:location]
-  @semantic_tags = SemanticTag.search(params[:semantic_tag]).order("created_at DESC") 			
-  @location = Location.search(@location).order("created_at DESC") ## pulls location IDs
-  #if @location.present?
-  @videos = Video.select("id", "title", "author", "location_ID").where(:location_ID => @location)
-  #@videos = Video.search(params[:video_title]).order("created_at DESC")    
-  if @videos.empty?	
-    @video = Video.new
-    @video.title = params[:video_title]
-  	@video.author = params[:video_author]
-  	@new_location = Location.new
-    @new_location.location = params[:location]
-  	@new_location.save
-  	@video.save
-    @video.location_id = @new_location.id
-  	@annotation.video_id = @video.id
-    @annotation.location_id = @new_location.id
-  	@video.save
-  	@annotation.save
-
-  	if @semantic_tags.empty?
-  	  @new_tag = SemanticTag.new
-  		@new_tag.tag = params[:semantic_tag]
-  		@new_tag.save
-  		@annotation.tag_id = @new_tag.id
-  		@annotation.save
-  	else		
-  		for t in @semantic_tags
-  			@annotation.tag_id = t.id
-  			@annotation.save
-  		end #end for t
-  	end #end if @semantic_tags
-  else # if video is alrady present
-    for x in @videos
-  	  id_num = x.id
-  		loc_id = x.location_id	
-  		@annotation.video_id = id_num
-  		@annotation.location_id = loc_id
-  		@annotation.save
-				
-  		if @semantic_tags.empty?
-  		  @new_tag = SemanticTag.new
-  			@new_tag.tag = params[:semantic_tag]
-  			@new_tag.save
-  			@annotation.tag_id = @new_tag.id
-  			@annotation.save
-  		else		
-  			for t in @semantic_tags
-  				@annotation.tag_id = t.id
-  				@annotation.save
-  			end #end for t
-  		end #end if @semantic_tags		
-  	 end	#end for x
-    end #end if @videos
-    
-    #@ret = {}
-    #@ret[:id] = @annotation.id
-    #@ret[:status] = 200
-    #render :json => @ret
+  addAnnotation params
 end #end def editAnnotation
   
 ###### DELETE ANNOTATION  
@@ -232,19 +160,7 @@ def deleteAnnotation ## accepts annotation id
 	anno = Annotation.find_by(id: search_term)
 	anno.update(deprecated: true)
 
-	# @annotations = []
-  # @annotations = Annotation.search(search_term).order("created_at DESC")
-	# for a in @annotations
-	# 	@annotation = Annotation.select("deprecated").where(search_term => a.id)
-	# 	for x in @annotation
-  #   	x.deprecated = true
-	# 		x.save
-	#   end
-  # end
-  #@ret = {}
-  #@ret[:status] = 200
-  #render :json => @ret
-end #end def depreciateAnnotation
+end
   
   
 end #end module
