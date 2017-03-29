4fa4742bc08e08327c8b7f04da7e429ea4c02d9a
seieibob <joncole04412@gmail.com>
2017-03-29 12:56:47 -0400

    Add underscore

--- app/models/semantic_tag.rb
+++ app/models/semantic_tag.rb
@@ -2,7 +2,7 @@ class SemanticTag < ActiveRecord::Base
 
 ##  belongs_to :annotation
   has_many :tag_annotations
-  has many :annotations, :through => :tag_annotations
+  has_many :annotations, :through => :tag_annotations
 
   def self.search(search)
 	  #query to identify videos where the 'title' field matches the annotation ID
