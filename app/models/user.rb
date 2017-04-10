9b60a0dc2460e7cc85cfbbe9dafc3becae2f62d1
seieibob <joncole04412@gmail.com>
2017-04-10 18:58:06 -0400

    Remove private

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,14 @@ class User < ActiveRecord::Base
 
 	before_create :set_auth_token
 
-	private
-		def set_auth_token
-			return if token.present?
-			self.token = generate_auth_token
-		end
+	
+	def set_auth_token
+		return if token.present?
+		self.token = generate_auth_token
+	end
 
-		def generate_auth_token
-			SecureRandom.uuid.gsub(/\-/,'')
-		end
+	def generate_auth_token
+		SecureRandom.uuid.gsub(/\-/,'')
 	end
 
 	def self.search(search)
