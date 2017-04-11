eefcd5e3267b6bc16250d43cc46e1c8aed7bc37a
seieibob <joncole04412@gmail.com>
2017-04-11 15:48:50 -0400

    Update routes to use "api" naming

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
- # resources :annotations
- # The priority is based upon order of creation: first created -> highest priority.
+  # resources :annotations
+  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
-   
+  root 'welcome#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-   get 'search/' => 'search#index', as: :search
-   get 'searchtag/' => 'search#index', as: :searchtag
- #  get 'login/' => 'welcome#index'
-   #get 'user/' => 'users#new'
-   #get 'annotators/bork' => 'annotators#bork'
-	 get 'annotators/getAnnotationsByLocation' => 'api_session#getAnnotationsByLocation'
-	 post 'annotators/addAnnotation' => 'api_session#addAnnotation'
-   post 'annotators/editAnnotation' => 'api_session#editAnnotation'
-   delete 'annotators/deleteAnnotation' => 'api_session#deleteAnnotation'
-   post 'annotators/login' => 'api_session#login'
-   delete 'annotators/logout' => 'api_session#logout'
+  get 'search/' => 'search#index', as: :search
+  get 'searchtag/' => 'search#index', as: :searchtag
+  #  get 'login/' => 'welcome#index'
+  #get 'user/' => 'users#new'
+  #get 'annotators/bork' => 'annotators#bork'
+  get    'api/getAnnotationsByLocation' => 'api_session#getAnnotationsByLocation'
+  post   'api/addAnnotation' => 'api_session#addAnnotation'
+  post   'api/editAnnotation' => 'api_session#editAnnotation'
+  delete 'api/deleteAnnotation' => 'api_session#deleteAnnotation'
+  post   'api/login' => 'api_session#login'
+  delete 'api/logout' => 'api_session#logout'
 
-# Configures store endpoint in your app
-mount AnnotatorStore::Engine, at: '/annotator_store'
+  # Configures store endpoint in your app
+  mount AnnotatorStore::Engine, at: '/annotator_store'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
@@ -30,59 +30,9 @@ mount AnnotatorStore::Engine, at: '/annotator_store'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
- # get '/annotation' => 'annotation#new'
+  # get '/annotation' => 'annotation#new'
   post '/annotation' => 'annotations#create'
 
   get '/root' => 'welcome#index'
 
-#get 'annotators/bork' => 'annotators#bork'
-#post 'annotators/newanno' => 'annotators#newanno'
-
-##############################
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
