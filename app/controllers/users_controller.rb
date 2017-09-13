541b5159add2f03aaba6c45aaec0cd273dff772f
colejd <joncole04412@gmail.com>
2017-09-13 15:02:02 -0400

    SHA1 user email and password from user creation form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def createFromForm
+    # SHA1 the email and password
+    email = Digest::SHA1.hexdigest :email
+    pass = Digest::SHA1.hexdigest :password
+    pass_conf = Digest::SHA1.hexdigest :password_confirmation
+
+    create(:name => :name, :email => email, :password => pass, :password_confirmation => pass_conf)
+  end
+
 private
 
   def user_params
