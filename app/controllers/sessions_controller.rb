c6817fef7d6a4826709dde5066d74adcd53cf74d
colejd <joncole04412@gmail.com>
2017-09-13 15:48:02 -0400

    Use SHA1 for login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-	  user = User.find_by_email(params[:email])
+    email = Digest::SHA1.hexdigest params[:email].to_s
+    pass = Digest::SHA1.hexdigest params[:password]
+
+	  user = User.find_by_email(email)
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(pass)
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
