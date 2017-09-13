2ae2050f1ebcb8c5736b1caf2575f97bb9723cd9
colejd <joncole04412@gmail.com>
2017-09-13 15:39:28 -0400

    Working on debug info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,15 @@ class UsersController < ApplicationController
   end
 
   def createFromForm
-    # SHA1 the email and password
-    email = Digest::SHA1.hexdigest :email.to_s
-    pass = Digest::SHA1.hexdigest :password.to_s
-    pass_conf = Digest::SHA1.hexdigest :password_confirmation.to_s
+    par = user_params
 
-    logger.info "Email: #{:email.to_s} => #{email}"
-    logger.info "Pass: #{:password.to_s} => #{pass}"
+    # SHA1 the email and password
+    email = Digest::SHA1.hexdigest par[:email].to_s
+    pass = Digest::SHA1.hexdigest par[:password].to_s
+    pass_conf = Digest::SHA1.hexdigest par[:password_confirmation].to_s
+
+    logger.info "Email: #{par[:email].to_s} => #{email}"
+    logger.info "Pass: #{par[:password].to_s} => #{pass}"
 
     user = User.new(:name => :name, :email => email, :password => pass, :password_confirmation => pass_conf)
     if user.save
